cb34463dc21f5c385b1839333de805713381a71f
Rish Chaudhary <72951402+rishchaudhary@users.noreply.github.com>
2023-09-20 18:57:09 -0400

    Login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -43,20 +43,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Learn React
-        </a>
-      </header>
+      <LoginPage/>
       <button onClick={newData}>TEST NEW DATA</button>
       <button onClick={updateData}>TEST UPDATE DATA</button>
     </div>
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,123 +1,42 @@
-import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
-// @mui
+import React from 'react';
+import { Card, CardContent, Typography, Button, IconButton, Link, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { Link, Container, Typography, Divider, Stack, Button, TextField } from '@mui/material';
-import Checkbox from '@mui/material/Checkbox';
-
-
 import Iconify from '../components/iconify';
+import Logo from '../assets/logo512.png'
 
-// ----------------------------------------------------------------------
-
-const StyledRoot = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
-  },
-}));
-
-const StyledSection = styled('div')(({ theme }) => ({
-  width: '100%',
-  maxWidth: 480,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  boxShadow: theme.customShadows.card,
-  backgroundColor: theme.palette.background.default,
-}));
-
-const StyledContent = styled('div')(({ theme }) => ({
-  maxWidth: 480,
+const StyledCard = styled(Card)({
+  maxWidth: 400,
   margin: 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0),
-}));
+  marginTop: 'calc(50vh - 150px)',
+  padding: 20,
+  textAlign: 'center',
+  borderRadius:10
+});
 
-// ----------------------------------------------------------------------
+const StyledButton = styled(Button)({
+   marginBottom:10,
+})
 
 export default function LoginPage() {
-
-  const [showPassword, setShowPassword] = useState(false);
+  const handleGoogleSignIn = () => {
+    // Implement Google Sign In Logic here
+  };
 
   return (
-    <>
-      <Helmet>
-        <title> Login | Minimal UI </title>
-      </Helmet>
-
-      <StyledRoot>
-
-
-        (
-        <StyledSection>
-          <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Hi, Welcome Back
+    <StyledCard>
+      <CardContent>
+      <img src={Logo} alt="Logo" width={80}/>
+      <br></br>
+        <StyledButton>
+        <Button variant="outlined" startIcon={<Iconify icon="eva:google-fill" color="#DF3E30" width={30} height={30} />}>
+          Sign in with Google
+        </Button>
+        </StyledButton>
+        <br></br>
+        <Typography component={Link} to="/register" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
+            Don&apos;t have an account?
           </Typography>
-          <img src="/assets/illustrations/illustration_login.png" alt="login" />
-        </StyledSection>
-        )
-
-        <Container maxWidth="sm">
-          <StyledContent>
-            <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
-            </Typography>
-
-            <Typography variant="body2" sx={{ mb: 5 }}>
-              Don’t have an account? {''}
-              <Link variant="subtitle2">Get started</Link>
-            </Typography>
-
-            <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:facebook-fill" color="#1877F2" width={22} height={22} />
-              </Button>
-
-              <Button fullWidth size="large" color="inherit" variant="outlined">
-                <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={22} height={22} />
-              </Button>
-            </Stack>
-
-            <Divider sx={{ my: 3 }}>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                OR
-              </Typography>
-            </Divider>
-
-
-            <>
-              <Stack spacing={3}>
-                <TextField name="email" label="Email address" />
-
-                <TextField
-                  name="password"
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-
-                />
-              </Stack>
-
-              <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-                <Checkbox name="remember" label="Remember me" />
-                <Link variant="subtitle2" underline="hover">
-                  Forgot password?
-                </Link>
-              </Stack>
-
-
-            </>
-
-
-          </StyledContent>
-        </Container>
-      </StyledRoot>
-    </>
+      </CardContent>
+    </StyledCard>
   );
 }
