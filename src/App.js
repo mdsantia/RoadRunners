358bd267963960a31fa97d86192cd1476acd185f
AliAlDarwish <alialdarwish02@gmail.com>
2023-09-24 17:39:21 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import VehicleForm from './pages/VehicleForm';
+import { Route, Routes } from 'react-router-dom'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import HomePage from './pages/HomePage';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
+import LoginPage from './pages/LoginPage';
+import VehicleForm from './pages/VehicleForm';
+import HomePage from './pages/HomePage';
 
-function App({children}) {
-  const [data, setData] = useState({});
 
-
-  useEffect(() => {
-    axios.get('/api/data')
-      .then(response => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    }, []);
-    
-  const newData = () => {
-    axios.post('/api/newData')
-      .then(response => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-  const updateData = () => {
-    axios.post('/api/updateData')
-      .then(response => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
+function App() {
   return (
-    
     <div className="App">
-      <LoginPage/>
-      <HomePage></HomePage>
-      {/* <VehicleForm/> */}
-      <button onClick={newData}>TEST NEW DATA</button>
-      <button onClick={updateData}>TEST UPDATE DATA</button>
+      <Routes> 
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/vehicle" element={<VehicleForm />} />
+      </Routes>
     </div>
   );
 }
