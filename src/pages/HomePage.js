3234b0663d238534eaccf6ecccb6053203e692c0
Rish Chaudhary <72951402+rishchaudhary@users.noreply.github.com>
2023-09-24 17:34:35 -0400

    Edited homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Card, CardContent, Typography, Button, IconButton, Link, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import DeleteIcon from '@mui/icons-material/Delete';
 import SearchIcon from '@mui/icons-material/Search';
 import image from '../assets/login-bg.jpg'
-import Logo from '../assets/roadrunner-logo.png'
+import Logo from '../assets/roadrunner-logo-clear.png'
+import Fab from '@mui/material/Fab';
 
 
 
@@ -31,15 +27,17 @@ const StyledCard = styled(Card)({
 });
 
 
-
-
 const ImageCard = styled(Card)({
-    maxWidth:2000,
-    width:1200,
+    position: 'relative',
+    maxWidth: 2000,
+    width: 1200,
     padding: 5,
-    height:500,
-    borderRadius:15,
+    height: 500,
+    borderRadius: 15,
+
+    
   });
+
 
 const StyledButton = styled(Button)({
    marginBottom:10,
@@ -47,20 +45,19 @@ const StyledButton = styled(Button)({
 
 export default function HomePage() {
 
-
   return (
     <div style={{backgroundColor:'#00455A', height:'100vh'}}>
       <Container>
-      <img src={Logo} style={{justifyContent:'left'}}alt="Logo" width={300}/>
+      <img src={Logo} alt="Logo" width={300} style={{ position: 'absolute', top: '6px', left: '20px' }} />
             <ImageCard>
               <StyledCard>
                   <TextField id="standard-basic" label="Starting Location" variant="standard" />
                   <TextField id="standard-basic" label="Start Date" variant="standard" />
                   <TextField id="standard-basic" label="End Date" variant="standard" />
                   <TextField id="standard-basic" label="Destination" variant="standard" />
-                  <IconButton aria-label="delete" style={{marginTop:'10px', marginLeft:'20px', backgroundColor:'#e0c404', color:'white'}}> 
-                      <SearchIcon fontsize="large"/>
-                  </IconButton>
+                  <Fab aria-label="delete" style={{marginTop:'10px', marginLeft:'20px', backgroundColor:'#e0c404', color:'white'}}> 
+                      <SearchIcon/>
+                  </Fab>
               </StyledCard>
               </ImageCard>
     </Container>
