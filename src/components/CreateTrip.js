6b8aac207235069607ce0f57db28dcda80b7273b
Rish Chaudhary <72951402+rishchaudhary@users.noreply.github.com>
2023-09-27 15:21:03 -0400

    Added address search

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,34 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
 import { Card } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import Fab from '@mui/material/Fab';
-
+import AddressSearch from './AddressSearch';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 const StyledCard = styled(Card)({
-  width: 800,
+  width: 1000,
   margin: 'auto',
-  padding: 14,
-  marginTop: 30,
+  padding: 20,
+  marginTop: 20,
   textAlign: 'center',
-  borderRadius: 40,
+  borderRadius: 20,
   boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
 });
 
 
 export default function HomePage() {
 
+  const [value, setValue] = useState(null);
+
   return (
     <StyledCard>
-      <TextField id="standard-basic" label="Starting Location" variant="standard" />
-      <TextField id="standard-basic" label="Start Date" variant="standard" />
-      <TextField id="standard-basic" label="End Date" variant="standard" />
-      <TextField id="standard-basic" label="Destination" variant="standard" />
-      <Fab aria-label="delete" style={{ marginTop: '10px', marginLeft: '20px', backgroundColor: 'red', color: 'white' }}>
-        <SearchIcon />
-      </Fab>
+      <Stack direction="row" spacing={2}>
+        <AddressSearch ></AddressSearch>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Start Date" />
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker label="End Date" />
+        </LocalizationProvider>
+        <AddressSearch></AddressSearch>
+        <Fab aria-label="delete" style={{ marginLeft: '20px', backgroundColor: 'red', color: 'white' }}>
+          <SearchIcon />
+        </Fab>
+      </Stack>
     </StyledCard>
   );
 }
