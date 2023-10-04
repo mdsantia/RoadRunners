a59aa715d8cff5962d79ec509cbcf68bb3f63483
Rish Chaudhary <72951402+rishchaudhary@users.noreply.github.com>
2023-10-04 14:23:28 -0400

    updated topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,14 +3,9 @@ import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Logo from '../assets/rr-logo.png'
 import { useNavigate } from 'react-router-dom';
@@ -18,8 +13,6 @@ import { useUserContext } from '../hooks/useUserContext';
 
 
 function TopBar() {
-
-  const profile_picture = React.useState("/static/images/avatar/2.jpg");
   const user = useUserContext();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -74,15 +67,12 @@ function TopBar() {
               <MenuItem onClick={handleProfile}>Profile</MenuItem>
               <MenuItem onClick={handleLogout}>Logout</MenuItem>
             </Menu>
-
           {/*show when not signed */}
           <Button
           OnClick={handleSignup}
           >Sign Up
           </Button>
-
           </Box>
-
         </Toolbar>
       </Container>
     </AppBar>
