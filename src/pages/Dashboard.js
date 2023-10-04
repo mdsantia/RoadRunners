e327945674f3203ec5db3e15267de6acd1bfce5b
Rish Chaudhary <72951402+rishchaudhary@users.noreply.github.com>
2023-10-04 14:21:08 -0400

    updated topbar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Card, Button, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import TopBar from '../components/TopBar';
-import CreateTrip from '../components/CreateTrip';
-import Map from '../components/Map'; // Import your Map component here
+import CreateTrip from '../components/CreateTrip'
 import { useState } from 'react';
 import Divider from '@mui/material/Divider';
 
@@ -12,44 +11,24 @@ const Container = styled('div')({
     justifyContent: 'left',
     alignItems: 'left',
     height: '80vh',
-    flexDirection: 'column',
-    position: 'relative', // Set position to relative
+    flexDirection: 'column'
 });
 
-const CreateTripContainer = styled('div')({
-    position: 'relative',
-    zIndex: 2, // Set a higher z-index for CreateTrip to make it appear above Map
-    backgroundColor: 'rgba(255, 255, 255, 0)', // Make CreateTripContainer semi-transparent
-});
 
-const MapWrapper = styled('div')({
-    width: '100%', // Set the width to 100%
-    height: '100%', // Set the height to 100%
-    position: 'absolute', // Set position to absolute
-    top: 0,
-    left: 0,
-});
+
 
 export default function Dashboard() {
+
     const [startLocation, setStartLocation] = useState("Chicago");
     const [endLocation, setEndLocation] = useState(null);
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
 
-    const [directionsResponse, setDirectionsResponse] = useState(null);
-
     return (
         <div style={{ backgroundColor: 'white', height: '100vh' }}>
             <TopBar></TopBar>
             <Container>
-                <CreateTripContainer>
-                    {/* Add your CreateTrip component here */}
-                    <CreateTrip />
-                </CreateTripContainer>
-                <MapWrapper>
-                    {/* Add your Map component here */}
-                    <Map directionsResponse={directionsResponse}/>
-                </MapWrapper>
+            <CreateTrip></CreateTrip>
             </Container>
         </div>
     );
