8a678102bb7ea7936b48e1f8ab903521a0f40d90
mdsantia1720 <82775895+mdsantia1720@users.noreply.github.com>
2023-10-05 02:26:59 -0400

    Changed Load of Maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, useJsApiLoader, DirectionsRenderer } from "@react-google-maps/api";
+import { GoogleMap, DirectionsService, DirectionsRenderer, LoadScript } from '@react-google-maps/api';
+import { DirectionContextProvider } from '../context/DirectionContext';
+import { useDirectionContext } from '../hooks/useDirectionContext';
 import { GOOGLE_MAPS_API_KEY } from './AddressSearch';
 
 export default function Map(props) {
     const [userLocation, setUserLocation] = useState(null);
-    const { isLoaded, loadError } = useJsApiLoader({
-        googleMapsApiKey: GOOGLE_MAPS_API_KEY,
-        // Move the nonce and libraries options to a separate object
-        // and pass them as dependencies to ensure they don't change
-        // between renders.
-        dependencies: {
-          libraries: ['places'],
-          nonce: props.nonce,
+    const { direction, setDirection } = useDirectionContext();
+    
+    const directionsCallback = (response) => {
+      if (response !== null) {
+        if (response.status === 'OK') {
+          // Render the directions on the map
+          return <DirectionsRenderer directions={response} options={{
+            polylineOptions: {
+              strokeColor: 'blue'
+            }}}/>;
+        } else {
+          console.error(`Directions request failed due to ${response.status}`);
         }
-    })
+      }
+    };
 
     useEffect(() => {
         if (navigator.geolocation && !userLocation) {
@@ -26,14 +33,11 @@ export default function Map(props) {
         }
     }, []);
 
-    if (!isLoaded) {
-        return <div>Loading...</div>;
-    }
-
     return (
+      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places', 'map']}>
         <GoogleMap
           center={userLocation || { lat: 40.43855441888486, lng: -86.91319150336594 }}
-          zoom={5}
+          zoom={12}
           mapContainerStyle={{ width: '100%', height: '100%' }}
           options={{
             streetViewControl: false,
@@ -41,13 +45,15 @@ export default function Map(props) {
             fullscreenControl: false
           }}
         >
-          {props.directionsResponse ? (
-            <>
-              <DirectionsRenderer directions={props.directionsResponse} />
-            </>
-          ) : (
-            0
-          )}
+          <DirectionsService
+            options={{
+              destination: 'Los Angeles, CA',
+              origin: 'San Francisco, CA',
+              travelMode: 'DRIVING'
+            }}
+            callback={directionsCallback}
+          />
         </GoogleMap>
+      </LoadScript>
     );
 }
