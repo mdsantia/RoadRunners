8db37502715a151c8fcd88f3762d78ea870c12ad
Jenny Ha <jpha@purdue.edu>
2023-10-14 23:14:30 -0400

    wrapped everything in theme and moved files around

--- src/App.js
+++ src/App.js
@@ -5,24 +5,43 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import UserProfile from './pages/UserProfile';
 import Dashboard from './pages/Dashboard';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Routes> 
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/login" element={<LoginPage />} />
-        <Route exact path="/profile/:pageType/:id" element={<UserProfile />} />
-        <Route exact path="/dashboard/:startLocation/:endLocation/:startDate/:endDate" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/" replace />}/>
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Routes> 
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/profile/:pageType/:id" element={<UserProfile />} />
+          <Route exact path="/dashboard/:startLocation/:endLocation/:startDate/:endDate" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />}/>
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
